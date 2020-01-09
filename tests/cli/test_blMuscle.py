75100f5f02c5275ff1ff3a6c9fed71623510ef8d
Bryce Besler <babesler@ucalgary.ca>
2020-01-09 15:42:58 -0700

    Read CSV during test for numerical precision

--- tests/cli/test_blMuscle.py
+++ tests/cli/test_blMuscle.py
@@ -2,8 +2,9 @@
 
 import unittest
 import subprocess
-import shutil, tempfile, filecmp
+import shutil, tempfile
 import os
+import numpy.testing as npt
 
 from .config_cli import cfg
 from bonelab.cli.Muscle import Muscle
@@ -51,7 +52,7 @@ class TestblMuscle(unittest.TestCase):
 
     def test_blMuscle(self):
         '''Can run `blMuscle`
-        
+
         Note that this test takes about 65 seconds, so we should only run it once.
         In order to save time, all tests are compressed here.'''
         # All outputs exist
@@ -61,12 +62,31 @@ class TestblMuscle(unittest.TestCase):
         self.assertTrue(os.path.isfile(self.args['segmentation_filename']))
 
         # CSV file written correctly
-        expected_filename = os.path.join(self.test_dir, 'muscle_expected.csv')
-        with open(expected_filename, 'w') as f:
-            f.write('Filename,Spacing.X [mm],Spacing.Y [mm],Spacing.Z [mm],density_slope,density_intercept,muscle density [native],A.Cross [vox^2]' + os.linesep)
-            f.write(self.args['input_filename']+',0.18219922482967377,0.18219922482967377,0.18219999969005585,0.1957420703125,-391.209015,2093.1338525656342,118632.01818181819' + os.linesep)
+        entry = []
+        header = []
+        with open(self.args['csv_filename'], 'r') as fp:
+            for cnt, line in enumerate(fp):
+                if len(header)==0:
+                    header = [x.replace(os.linesep, '') for x in line.split(',')]
+                else:
+                    this_entry = [x.replace(os.linesep, '') for x in line.split(',')]
+                    entry.append(this_entry)
 
-        self.assertTrue(filecmp.cmp(expected_filename, self.args['csv_filename']))
+        expected_header= [
+            'Filename', 'Spacing.X [mm]', 'Spacing.Y [mm]', 'Spacing.Z [mm]',
+            'density_slope', 'density_intercept',
+            'muscle density [native]', 'A.Cross [vox^2]'
+        ]
+        self.assertEqual(len(entry), 1)
+        self.assertEqual(header, expected_header)
+        self.assertEqual(entry[0][0], self.args['input_filename'])
+        npt.assert_almost_equal(float(entry[0][1]), 0.18219922482967377, decimal=4)
+        npt.assert_almost_equal(float(entry[0][2]), 0.18219922482967377, decimal=4)
+        npt.assert_almost_equal(float(entry[0][3]), 0.18219922482967377, decimal=4)
+        npt.assert_almost_equal(float(entry[0][4]), 0.1957420703125, decimal=4)
+        npt.assert_almost_equal(float(entry[0][5]), -391.209015, decimal=4)
+        npt.assert_almost_equal(float(entry[0][6]), 2093.1338525656342, decimal=4)
+        npt.assert_almost_equal(float(entry[0][7]), 118632.01818181819, decimal=4)
 
 
 if __name__ == '__main__':
