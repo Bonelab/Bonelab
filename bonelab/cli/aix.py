b5ba9aa7ae19ee1c340d480b6d61aed68cd607d2
Steven Boyd <skboyd@ucalgary.ca>
2021-05-18 21:24:27 -0600

    Added functionality to read nifti

--- bonelab/cli/aix.py
+++ bonelab/cli/aix.py
@@ -4,32 +4,41 @@ import argparse
 import os
 import vtkbone
 from vtk.util.numpy_support import vtk_to_numpy
+from bonelab.io.vtk_helpers import get_vtk_reader
 import numpy as np
 import re
+import math
 
-def aix(aim_file, log, stat, histo, verbose, meta):
+# vtkNIFTIImageReader
+# vtkboneAIMReader
+
+def aix(infile, log, stat, histo, verbose, meta):
     # Python 2/3 compatible input
     from six.moves import input
 
     debug = False
     
-    # Read input file
-    if not os.path.isfile(aim_file):
-        print("!%  Can't open file {} for reading".format(aim_file))
-        print("File read error: {}".format(aim_file))
-    reader = vtkbone.vtkboneAIMReader()
-    reader.DataOnCellsOff()
-    reader.SetFileName(aim_file)
+    # Read input
+    if not os.path.isfile(infile):
+        os.sys.exit('[ERROR] Cannot find file \"{}\"'.format(input_filename))
+
+    reader = get_vtk_reader(infile)
+    if reader is None:
+        os.sys.exit('[ERROR] Cannot find reader for file \"{}\"'.format(input_filename))
+
+    #print('Reading input image ' + infile)
+    reader.SetFileName(infile)
     reader.Update()
     image = reader.GetOutput()
 
     # Precompute some values
     guard = '!-------------------------------------------------------------------------------'
-    phys_dim = [x*y for x,y in zip(image.GetDimensions(), reader.GetElementSize())]
-    size = os.path.getsize(aim_file)
+    phys_dim = [x*y for x,y in zip(image.GetDimensions(), image.GetSpacing())]
+    position = [math.floor(x/y) for x,y in zip(image.GetOrigin(), image.GetSpacing())]
+    size = os.path.getsize(infile)
     names = ['Bytes', 'KBytes', 'MBytes', 'GBytes']
     n_image_voxels = image.GetDimensions()[0] * image.GetDimensions()[1] * image.GetDimensions()[2]
-    voxel_volume = reader.GetElementSize()[0] * reader.GetElementSize()[1] * reader.GetElementSize()[2]
+    voxel_volume = image.GetSpacing()[0] * image.GetSpacing()[1] * image.GetSpacing()[2]
     i = 0
     while int(size) > 1024 and i < len(names):
         i+=1
@@ -42,8 +51,8 @@ def aix(aim_file, log, stat, histo, verbose, meta):
       print('!>')
       print('!> dim                            {: >6}  {: >6}  {: >6}'.format(*image.GetDimensions()))
       print('!> off                                 x       x       x')
-      print('!> pos                            {: >6}  {: >6}  {: >6}'.format(*reader.GetPosition()))
-      print('!> element size in mm             {:.4f}  {:.4f}  {:.4f}'.format(*reader.GetElementSize()))
+      print('!> pos                            {: >6}  {: >6}  {: >6}'.format(*position))
+      print('!> element size in mm             {:.4f}  {:.4f}  {:.4f}'.format(*image.GetSpacing()))
       print('!> phys dim in mm                 {:.4f}  {:.4f}  {:.4f}'.format(*phys_dim))
       print('!>')
       print('!> Type of data               {}'.format(image.GetScalarTypeAsString()))
@@ -52,14 +61,18 @@ def aix(aim_file, log, stat, histo, verbose, meta):
 
     # Print log
     if log:
-        print(reader.GetProcessingLog())
+        if (reader.GetClassName() == "vtkboneAIMReader"):
+          print(reader.GetProcessingLog())
+        else:
+          print('!- No log available.')
 
     # Print meta data information
     if meta:
-        log = reader.GetProcessingLog()
+        if (reader.GetClassName() == "vtkboneAIMReader"):
+          log = reader.GetProcessingLog()
         
         if (not log):
-          print("None")
+          print('!- No meta data available.')
           exit(1)
           
         meta_list=[]
@@ -140,17 +153,31 @@ def aix(aim_file, log, stat, histo, verbose, meta):
     # Print Histogram
     if histo:
         array = vtk_to_numpy(image.GetPointData().GetScalars()).ravel()
+
+        # The range of values and number of bins are hard-coded. If fewer bins
+        # or a different range (i.e. 0 to 127 for char) are wanted then simply 
+        # adjust these settings
+        if (image.GetScalarTypeAsString()=="char"):
+          nRange = [-128,127]
+          nBins = 128
+        elif (image.GetScalarTypeAsString()=="short"):
+          nRange = [-32768,32767]
+          nBins = 128
+        else:
+          print('!- Unknown data type: {}'.format(image.GetScalarTypeAsString()))
+      
         # https://numpy.org/doc/stable/reference/generated/numpy.histogram.html
-        nSpan = 128
-        nBuckets = 128
-        nWidth = 60
-        fill,bins = np.histogram(array,nBuckets,[0,127],None,None,True)
-        print('!>  {:4s} ({:.3s}) : {:s}'.format('IND','QTY','Size'))
-        for index in range(nBuckets):
-          normalized_scale = fill[index]*nSpan/nBuckets
-          nStars = int(normalized_scale*nWidth)
-          factor = nSpan/nBuckets
-          print('!> {:4d} ({:.3f}): {:s}'.format(index,normalized_scale,nStars*'*'))
+        hist,bin_edges = np.histogram(array,nBins,nRange,None,None,False)
+        nValues = sum(hist)
+
+        print('!>  {:4s} ({:.3s}) : Showing {:d} histogram bins over range of {:d} to {:d}.'.format('IND','QTY',nBins,*nRange))
+        for bin in range(nBins):
+          index = nRange[0] + int(bin * (nRange[1]-nRange[0])/(nBins-1))
+          count = hist[bin]/nValues # We normalize so total count = 1
+          nStars = int(count*100)
+          if (nStars > 60):
+            nStars = 60 # just prevents it from wrapping in the terminal
+          print('!> {:4d} ({:.3f}): {:s}'.format(index,count,nStars*'*'))
         print(guard)
         
     # Print verbose
@@ -176,26 +203,32 @@ def aix(aim_file, log, stat, histo, verbose, meta):
             i+=1
 
 def main():
-    # Setup argument parsing
-    parser = argparse.ArgumentParser(
-        prog="aix",
-        description='''AIM information examine
+  
+    # Setup description
+    description='''AIM information examine.
 
 A reimplementation of the seminal SCANCO program `aix`. All
 efforts have been made to preserve the exact output beyond
 the description but some differences are bound to arise.
+
+Allowed inputs include .aim, .nii
 '''
+    # Setup argument parsing
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawTextHelpFormatter,
+        prog="aix",
+        description=description
     )
-    parser.add_argument('aim_file', help='Input aim')
-    parser.add_argument('--log', '--l', action='store_true', help='show processing log')
-    parser.add_argument('--stat', '--s', action='store_true', help='show statistics on data')
+    parser.add_argument('infile', help='Input file')
+    parser.add_argument('--log', action='store_true', help='show processing log')
+    parser.add_argument('--stat', action='store_true', help='show statistics on data')
     parser.add_argument('--histo', action='store_true', help='show histogram of data')
-    parser.add_argument('--verbose', '--v', action='store_true', help='show data values')
-    parser.add_argument('--meta', '--m', action='store_true', help='show basic scan meta data')
+    parser.add_argument('--verbose', action='store_true', help='show data values')
+    parser.add_argument('--meta', action='store_true', help='show scan meta data')
 
     # Parse and display
     args = parser.parse_args()
-
+    
     # Run program
     aix(**vars(args))
 
