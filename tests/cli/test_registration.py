277c00bca888e267d95c73a9dd4c007f52439e2f
Nathan <njneeteson@gmail.com>
2023-02-19 00:32:50 -0700

    start writing tests

---
+++ tests/cli/test_registration.py
@@ -0,0 +1,18 @@
+from __future__ import annotations
+
+import unittest
+from hypothesis import given, strategies as st
+
+from bonelab.cli.registration import create_parser, registration
+
+
+class TestRegistration(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_default(self):
+        pass
