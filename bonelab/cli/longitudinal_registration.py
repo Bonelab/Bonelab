7e5f801504129d4565b865a9673390cab5493113
Nathan <njneeteson@gmail.com>
2023-07-24 15:19:22 -0600

    initial implementation of longitudinal reg

--- bonelab/cli/longitudinal_registration.py
+++ bonelab/cli/longitudinal_registration.py
@@ -1,15 +1,120 @@
 from __future__ import annotations
 
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter, Namespace
+import os
+
+import SimpleITK
 
 from bonelab.util.registration_util import (
     create_file_extension_checker, create_string_argument_checker, INTERPOLATORS,
-    check_percentage
+    INPUT_EXTENSIONS, TRANSFORM_EXTENSIONS, check_percentage, get_output_base, write_args_to_yaml, check_inputs_exist,
+    check_for_output_overwrite, write_metrics_to_csv, create_and_save_metrics_plot, read_and_downsample_image,
+    setup_optimizer, setup_similarity_metric, setup_interpolator, setup_transform, setup_multiscale_progression,
+    check_image_size_and_shrink_factors, message_s
 )
 
 
 def longitudinal_registration(args: Namespace):
-    pass
+    """
+    Performs rigid longitudinal registration on a series of images. Intended for a time series but can also be used
+    for a series of repeat scans from the same subject for precision.
+
+    Parameters
+    ----------
+    args : Namespace
+        The parsed command line arguments.
+
+    Returns
+    -------
+    None
+    """
+    check_inputs_exist([args.baseline_image] + args.follow_up_images, args.silent)
+    output_common_region_fn = os.path.join(args.output_directory, f"{args.output_label}_common_region.nii.gz")
+    output_transformation_fns = [
+        os.path.join(args.output_directory, f"{args.output_label}_transformation_{i}.txt")
+        for i in range(len(args.follow_up_images))
+    ]
+    output_yaml_fn = os.path.join(args.output_directory, f"{args.output_label}.yaml")
+    check_for_output_overwrite(
+        [output_yaml_fn, output_common_region_fn] + output_transformation_fns,
+        args.overwrite, args.silent
+    )
+    write_args_to_yaml(output_yaml_fn, args, args.silent)
+    baseline_image = read_and_downsample_image(
+        args.baseline_image, "baseline",
+        args.downsampling_shrink_factor, args.downsampling_smoothing_sigma,
+        args.silent
+    )
+    common_region = sitk.Add(sitk.Image(*baseline_image.GetSize(), sitk.sitkUInt8), 1)
+    registration_method = sitk.ImageRegistrationMethod()
+    registration_method = setup_optimizer(
+        registration_method,
+        args.max_iterations,
+        args.gradient_descent_learning_rate,
+        args.gradient_descent_convergence_min_value,
+        args.gradient_descent_convergence_window_size,
+        args.powell_max_line_iterations,
+        args.powell_step_length,
+        args.powell_step_tolerance,
+        args.powell_value_tolerance,
+        args.optimizer,
+        args.silent
+    )
+    registration_method = setup_similarity_metric(
+        registration_method,
+        args.similarity_metric,
+        args.mutual_information_num_histogram_bins,
+        args.joint_mutual_information_joint_smoothing_variance,
+        args.similarity_metric_sampling_strategy,
+        args.similarity_metric_sampling_rate,
+        args.similarity_metric_sampling_seed,
+        args.silent
+    )
+    registration_method = setup_interpolator(registration_method, args.interpolator, args.silent)
+    registration_method = setup_multiscale_progression(
+        registration_method,
+        args.shrink_factors, args.smoothing_sigmas,
+        args.silent
+    )
+    metric_callback = MetricTrackingCallback(registration_method, args.silent, False)
+    registration_method.AddCommand(sitk.sitkIterationEvent, metric_callback)
+    for i, (follow_up_image_fn, transform_fn) in enumerate(zip(args.follow_up_images, output_transformation_fns)):
+        message_s(f"Processing follow-up {i}", args.silent)
+        follow_up_image = read_and_downsample_image(
+            follow_up_image_fn, f"follow-up {i}",
+            args.downsampling_shrink_factor, args.downsampling_smoothing_sigma,
+            args.silent
+        )
+        check_image_size_and_shrink_factors(
+            baseline_image, follow_up_image,
+            args.shrink_factors, args.silent
+        )
+        registration_method = setup_transform(
+            registration_method,
+            baseline_image, follow_up_image,
+            args.transform_type, args.centering_initialization,
+            args.silent
+        )
+        message_s("Starting registration", args.silent)
+        transform = registration_method.Execute(fixed_image, moving_image)
+        message_s(
+            f"Registration stopping condition: {registration_method.GetOptimizerStopConditionDescription()}",
+            args.silent
+        )
+        message_s(f"Writing transformation to {transform_fn}", args.silent)
+        sitk.WriteTransform(transform, transform_fn)
+        message_s("Transforming follow-up image to baseline space to update common region", args.silent)
+        common_region = SimpleITK.Multiply(
+            common_region,
+            sitk.Resample(
+                sitk.Add(sitk.Image(*baseline_image.GetSize(), sitk.sitkUInt8), 1),
+                baseline_image,
+                transform,
+                sitk.sitkNearestNeighbor
+            )
+        )
+
+
 
 
 def create_parser() -> ArgumentParser:
@@ -18,6 +123,7 @@ def create_parser() -> ArgumentParser:
                     "can also be used for a series of repeat scans from the same subject for precision. The first "
                     "input is the baseline image. The second input is a list of follow-up images. You must also "
                     "specify a set of parameters for the rigid registrations, and finally an output directory. "
+                    "and output label."
                     "The output directory will contain a transformation file for each follow-up image, and a "
                     "common region mask in the reference frame of the baseline image.",
         formatter_class=ArgumentDefaultsHelpFormatter
@@ -30,6 +136,12 @@ def create_parser() -> ArgumentParser:
     parser.add_argument(
         "follow_up_images", nargs="+", type=create_file_extension_checker(INPUT_EXTENSIONS, "follow_up_images"),
         help=f"Provide follow-up image input filename ({', '.join(INPUT_EXTENSIONS)})"
+    )
+    parser.add_argument(
+        "output_directory", type=str, help="Provide output directory"
+    )
+    parser.add_argument(
+        "output_label", type=str, help="Provide output label"
     )
     parser.add_argument(
         "--overwrite", "-ow", default=False, action="store_true",
